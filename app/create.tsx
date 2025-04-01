5eb91c996e3179ac25792d76263b3956b456fbeb
elisacomposta <elisa.composta.a@gmail.com>
2025-04-01 21:26:48 +0200

    Navigate to create event page

---
+++ app/create.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import styles from './styles/CreateEvent.styles';
+import { Text } from 'react-native';
+
+export default function CreateEvent() {
+    return (
+        <SafeAreaView>
+            <Text>Create event page</Text>
+        </SafeAreaView>
+    );
+}
--- components/OptionButton.tsx
+++ components/OptionButton.tsx
@@ -1,9 +1,12 @@
 import { Text, TouchableOpacity } from "react-native";
 import styles from "./OptionButton.styles";
+import { useRouter } from "expo-router";
 
 export function OptionButton({ symbol }: { symbol?: string }) {
+    const router = useRouter();
+
     return (
-        <TouchableOpacity style={styles.createButton} onPress={() => console.log("Create new")}>
+        <TouchableOpacity style={styles.createButton} onPress={() => router.push('/create')}>
             <Text style={styles.createText}>{symbol}</Text>
         </TouchableOpacity>
     );
